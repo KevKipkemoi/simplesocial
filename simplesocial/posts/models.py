806be9e230e21c2f39b97c72a60db8d0bf8f4324
KevKipkemoi <kevkipkemoi@outlook.com>
2020-02-06 21:19:48 +0300

    fix

--- simplesocial/posts/models.py
+++ simplesocial/posts/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
 		super().save(*args, **kwargs)
 
 	def get_absolute_url(self):
-		return reverse('psosts:single', kwargs = {'username': self.user.username, 'pk': self.pk})
+		return reverse('posts:single', kwargs = {'username': self.user.username, 'pk': self.pk})
 
 	class Meta:
 		ordering = ['-created_at']
